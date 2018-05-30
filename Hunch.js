34477ab5009bba3208df6431a3ec83e6f084cb78
LeaveTheCapital <hellohenry@gmail.com>
2018-05-30 12:48:31 +0100

    N with jaunty angle

--- Hunch.js
+++ Hunch.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View, Animated, Easing } from "react-native";
 import Svg, { Circle, Line, Path } from "react-native-svg";
-import H from './H.js';
-import U from './U.js';
-import N from './N.js';
-import C from './C.js';
+import H from "./H.js";
+import U from "./U.js";
+import N from "./N.js";
+import C from "./C.js";
 
 const A = {
   Line: Animated.createAnimatedComponent(Line),
@@ -13,20 +13,43 @@ const A = {
 };
 
 export default class Hunch extends React.Component {
- 
-
   render() {
-
     const { height, initialCoordinates } = this.props;
 
-
-    return (<Svg height="280" width="400">
-          <H fill="yellow" stroke="black" height={height} initialCoordinates={initialCoordinates} />
-          <U fill="red" stroke="black" height={height} initialCoordinates="100 50" />
-          <N fill="mediumvioletred" stroke="black" height={height} initialCoordinates="170 50" />
-          <C fill="whitesmoke" stroke="black" height={height} initialCoordinates="240 70" />
-          <H fill="powderblue" stroke="black" height={height} initialCoordinates="310 50" />
-        </Svg>)
+    return (
+      <Svg height="280" width="400">
+        <H
+          fill="yellow"
+          stroke="black"
+          height={height}
+          initialCoordinates={initialCoordinates}
+        />
+        <U
+          fill="red"
+          stroke="black"
+          height={height}
+          initialCoordinates="100 50"
+        />
+        <N
+          fill="mediumvioletred"
+          stroke="black"
+          height={height}
+          initialCoordinates="170 50"
+        />
+        <C
+          fill="whitesmoke"
+          stroke="black"
+          height={height}
+          initialCoordinates="240 70"
+        />
+        <H
+          fill="cornflowerblue"
+          stroke="black"
+          height={height}
+          initialCoordinates="310 50"
+        />
+      </Svg>
+    );
   }
 }
-9940076670395700
+9940076670395700;
